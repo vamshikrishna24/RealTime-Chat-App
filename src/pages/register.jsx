7fc835abf3612d824e0d1cb8f242816f1d55dd48
vamshikrishna24 <vamshikrishna2124@gmail.com>
2022-12-29 14:11:07 +0530

    Specified Register details

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import add from "../img/imgupload.png";
+import { auth, storage, db } from "../firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+
 const Register = () => {
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      // console.log(res);
+
+      const storageRef = ref(storage, displayName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+
+            await setDoc(doc(db, "userChats", res.user.id), {});
+          });
+        }
+      );
+    } catch (err) {
+      setErr(true);
+    }
+  };
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Chat World</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="display name" />
           <input type="email" placeholder="email" />
           <input type="password" placeholder="password" />
@@ -15,6 +60,7 @@ const Register = () => {
             <img src={add} alt="" /> <span>Add an avatar</span>
           </label>
           <button>Sign Up</button>
+          {err && <span>Something went wrong.</span>}
         </form>
         <p>Already have an account? Login</p>
       </div>
