c9423628165c1079f77fd21b798a67d87cab0b4c
vamshikrishna24 <vamshikrishna2124@gmail.com>
2022-12-29 10:19:09 +0530

    Added chatinfo

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import video from "../img/video.png";
+import person from "../img/person-add-4.png";
+import more from "../img/more.png";
+import Messages from "./messages";
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <img src={video} alt="" />
+          <img src={person} alt="" />
+          <img src={more} alt="" />
+        </div>
+      </div>
+      <Messages />
+    </div>
+  );
 };
 
 export default Chat;
